4d6d8dad10c9925564fe87679780f4ceb8f84e95
McTechDev <thatonefurryreal@gmail.com>
2025-06-18 14:30:32 -0400

    Create channels-getter-shorts.py

---
+++ channels-getter-shorts.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+import os
+import json
+
+options = Options()
+options.add_argument("--disable-gpu")
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.youtube.com/shorts")
+
+time.sleep(5)
+
+print("\nGetting elements...\n\n")
+
+final_channels = {}
+i = 0
+
+for i in range(50):
+    i += 1
+    print(f"Getting channel ({str(i)})")
+    channelName = driver.find_element(By.CSS_SELECTOR, 'a.yt-core-attributed-string__link.yt-core-attributed-string__link--call-to-action-color.yt-core-attributed-string--link-inherit-color')
+    pfpElement = driver.find_element(By.CSS_SELECTOR, 'img.yt-core-image.yt-spec-avatar-shape__image.yt-core-image--fill-parent-height.yt-core-image--fill-parent-width.yt-core-image--content-mode-scale-to-fill.yt-core-image--loaded')
+    channelPfp = pfpElement.get_attribute('src')
+    print(f"Channel handle: {channelName.text} | Channel PFP: {channelPfp}\n")
+    final_channels[channelName.text] = channelPfp
+    time.sleep(1)
+    if i == 1:
+        nextBtn = driver.find_element(By.CSS_SELECTOR, 'button.yt-spec-button-shape-next.yt-spec-button-shape-next--text.yt-spec-button-shape-next--mono.yt-spec-button-shape-next--size-xl.yt-spec-button-shape-next--icon-button.yt-spec-button-shape-next--enable-backdrop-filter-experiment')
+        nextBtn.click()
+    else:
+        nextBtnList = driver.find_elements(By.CSS_SELECTOR, 'button.yt-spec-button-shape-next.yt-spec-button-shape-next--text.yt-spec-button-shape-next--mono.yt-spec-button-shape-next--size-xl.yt-spec-button-shape-next--icon-button.yt-spec-button-shape-next--enable-backdrop-filter-experiment')
+        nextBtn = nextBtnList[1]
+        nextBtn.click()
+
+print("\nCreated dictionary, dumping channels to channels.json...")
+with open("channels.json", "w") as f:
+    json.dump(final_channels, f, indent=4)
+    
+print("\nDone! :3")
+
+driver.quit()
