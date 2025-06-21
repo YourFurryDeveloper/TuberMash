bb91932b7a822ae502ec7f4e14f8cd1990ded4bb
McTechDev <thatonefurryreal@gmail.com>
2025-06-21 12:51:41 -0400

    Update channels-getter-socialblade.py

--- channels-getter-socialblade.py
+++ channels-getter-socialblade.py
@@ -17,6 +17,9 @@ driver.get("https://socialblade.com/youtube/lists/top/100/sb/games/US")
 # -----| SET THE AMOUNT OF CHANNELS HERE |-----
 channelsToGet = 100
 
+# Set debug on or off (Set to False if you're doing a big number.)
+debug_output = True
+
 time.sleep(5)
 
 css_selector_imgs = (
@@ -35,13 +38,13 @@ channelNames = driver.find_elements(By.CSS_SELECTOR, css_selector_names)
 channelImgs = driver.find_elements(By.CSS_SELECTOR, css_selector_imgs)
 
 for i in range(channelsToGet):
-    print(f"Getting channel ({str(i)})")
+    if debug_output: print(f"Getting channel ({str(i)})")
 
     pfpElement = channelImgs[i]
     channelName = channelNames[i]
 
     channelPfp = pfpElement.get_attribute('src')
-    print(f"Channel handle: {channelName.text} | Channel PFP: {channelPfp}\n")
+    if debug_output: print(f"Channel handle: {channelName.text} | Channel PFP: {channelPfp}\n")
     final_channels[channelName.text] = channelPfp
 
 print("\nCreated dictionary, dumping channels to channels.json...")
