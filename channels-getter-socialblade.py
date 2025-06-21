de9bcab05f87d0678d1f15f8b2443f9d16ced850
McTechDev <thatonefurryreal@gmail.com>
2025-06-21 12:40:10 -0400

    Create channels-getter-socialblade.py

---
+++ channels-getter-socialblade.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import time
+import os
+import json
+
+options = Options()
+options.add_argument("--disable-gpu")
+driver = webdriver.Chrome(options=options)
+driver.get("https://socialblade.com/youtube/lists/top/250/sb/all/US")
+
+# -----| SET THE AMOUNT OF CHANNELS HERE |-----
+channelsToGet = 100
+
+time.sleep(5)
+
+css_selector_imgs = (
+    ".inline-flex.ml-3"
+)
+
+css_selector_names = (
+    ".px-4.h-full"
+)
+
+print("\nGetting elements...\n\n")
+
+final_channels = {}
+
+channelNames = driver.find_elements(By.CSS_SELECTOR, css_selector_names)
+channelImgs = driver.find_elements(By.CSS_SELECTOR, css_selector_imgs)
+
+for i in range(channelsToGet):
+    print(f"Getting channel ({str(i)})")
+
+    pfpElement = channelImgs[i]
+    channelName = channelNames[i]
+
+    channelPfp = pfpElement.get_attribute('src')
+    print(f"Channel handle: {channelName.text} | Channel PFP: {channelPfp}\n")
+    final_channels[channelName.text] = channelPfp
+
+print("\nCreated dictionary, dumping channels to channels.json...")
+with open("channels.json", "w") as f:
+    json.dump(final_channels, f, indent=4)
+    
+print("\nDone! :3")
+
+driver.quit()
